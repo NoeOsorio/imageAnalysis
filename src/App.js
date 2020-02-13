528f8cee803ec55d33be6de6b5b6e4568c7443a7
Noe Osorio <noe.osorio@smartkode.mx>
2020-02-12 21:24:41 -0600

    image to server

--- src/App.js
+++ src/App.js
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Spinner from './Spinner'
+import Images from './Images'
+import Buttons from './Buttons'
+// import { API_URL } from './config'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const API_URL = "http://localhost:5000"
+const content_type = 'image/jpeg'
+
+export default class App extends Component {
+
+  state = {
+    uploading: false,
+    images: []
+  }
+
+  onChange = e => {
+    const files = Array.from(e.target.files)
+    this.setState({ uploading: true })
+
+    const formData = new FormData()
+
+    // files.forEach((file, i) => {
+    //   formData.append(i, file)
+    // })
+    
+    fetch(`${API_URL}/api/image`, {
+      method: 'POST',
+      body: files[0],
+      headers:
+        { 'content-type': content_type }
+
+    })
+      .then(res => res.json())
+      .then(images => {
+        console.log(images)
+        this.setState({
+          uploading: false,
+          images
+        })
+      })
+  }
+
+  removeImage = id => {
+    this.setState({
+      images: this.state.images.filter(image => image.public_id !== id)
+    })
+  }
+
+  render() {
+    const { uploading, images } = this.state
+
+    const content = () => {
+      switch (true) {
+        case uploading:
+          return <Spinner />
+        case images.length > 0:
+          return <Images images={images} removeImage={this.removeImage} />
+        default:
+          return <Buttons onChange={this.onChange} />
+      }
+    }
+
+    return (
+      <div>
+        <div className='buttons'>
+          {content()}
+        </div>
+      </div>
+    )
+  }
 }
-
-export default App;
